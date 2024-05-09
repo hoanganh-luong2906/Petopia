6cc1b8e77be39ba3019131cd1f96bded3ea4d4f4
Hoang Anh <hoanganh.luong2906@gmail.com>
2024-05-09 13:44:04 +0700

    HomeScreen & Adjust functions

--- components/user/UserProfile.tsx
+++ components/user/UserProfile.tsx
@@ -1,16 +1,81 @@
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, View } from 'react-native';
 import CustomText from '../CustomText';
-import { FONT_REGULAR } from '../../utils/Types';
+import { FONT_BOLD, FONT_REGULAR, IUserProfile } from '../../utils/Types';
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const UserProfile = () => {
+	const [userData, setUserData] = useState<IUserProfile>({} as IUserProfile);
+
+	useFocusEffect(
+		useCallback(() => {
+			const getUserData = async () => {
+				try {
+					const api: string = process.env.SERVER_API_URL ?? '';
+					const token = (await AsyncStorage.getItem('token')) ?? '';
+
+					const response = await fetch(`${api}/user/userProfile`, {
+						method: 'GET',
+						headers: {
+							Authorization: `Bearer ${token}`,
+						},
+					});
+					console.log('>>> Response: ', JSON.stringify(response, null, 2));
+
+					if (response.ok) {
+						const data = await response.json();
+						const validUserData: IUserProfile = data.user ?? {};
+						console.log('PRO FILE: ', JSON.stringify(validUserData, null, 2));
+						if (validUserData) {
+							setUserData({ ...validUserData });
+						}
+					}
+				} catch (error: any) {
+					console.log('Error: ' + error);
+					alert('Error: ' + error.message);
+				}
+			};
+			getUserData();
+		}, [])
+	);
+
 	return (
 		<View style={styles.container}>
 			<ScrollView showsVerticalScrollIndicator={false}>
 				<View style={styles.profileContainer}>
+					<View style={styles.imageContainer}>
+						{(userData.avatarLink ? [0] : false) ? (
+							<Image
+								src={userData.avatarLink[0]}
+								resizeMode='cover'
+								style={styles.imageBackground}
+							/>
+						) : (
+							<Image
+								source={require('../../assets/images/default-bg.png')}
+								resizeMode='cover'
+								style={styles.imageBackground}
+							/>
+						)}
+						{(userData.avatarLink ? [1] : false) ? (
+							<Image
+								src={userData.avatarLink[1]}
+								resizeMode='cover'
+								style={styles.imageAvt}
+							/>
+						) : (
+							<Image
+								source={require('../../assets/images/default-avt.png')}
+								resizeMode='cover'
+								style={styles.imageAvt}
+							/>
+						)}
+					</View>
 					<CustomText
-						message='This is User Profile'
-						styles={{}}
-						variant={FONT_REGULAR}
+						message={userData.name}
+						styles={styles.userName}
+						variant={FONT_BOLD}
 					/>
 				</View>
 			</ScrollView>
@@ -26,7 +91,43 @@ const styles = StyleSheet.create({
 		overflow: 'hidden',
 		backgroundColor: '#ededed',
 	},
-	profileContainer: {},
+	profileContainer: {
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+	},
+	imageContainer: {
+		width: '100%',
+		height: 200,
+		position: 'relative',
+		overflow: 'hidden',
+	},
+	imageBackground: {
+		width: '100%',
+		height: 150,
+		borderRadius: 10,
+	},
+	imageAvt: {
+		position: 'absolute',
+		bottom: 0,
+		left: '50%',
+		transform: [{ translateX: -50 }],
+		width: 100,
+		height: 100,
+		overflow: 'hidden',
+		borderRadius: 100,
+		borderWidth: 5,
+		borderColor: 'white',
+	},
+	userName: {
+		width: '100%',
+		textAlign: 'center',
+		marginTop: 10,
+		fontSize: 25,
+		letterSpacing: 1,
+	},
 });
 
 export default UserProfile;
