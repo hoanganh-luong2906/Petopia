258ac4b82b1a79c3eb47d69a02057f7628b6cdb0
Hoang Anh <hoanganh.luong2906@gmail.com>
2024-06-24 05:18:28 +0700

    Admin Routes

--- navigations/AdminNavigation.tsx
+++ navigations/AdminNavigation.tsx
@@ -1,15 +1,54 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AdminHomeScreen from '../screens/admin/AdminHomeScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
+import AdminWaitingListScreen from '../screens/admin/AdminWaitingListScreen';
+import AdminNotificationScreen from '../screens/admin/AdminNotificationScreen';
+import AdminProfileScreen from '../screens/admin/AdminProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
 const AdminNavigation = () => {
 	return (
 		<Tab.Navigator
-			initialRouteName='admin-home'
+			initialRouteName='admin-dashboard'
 			screenOptions={{ headerShown: false, tabBarShowLabel: false }}
 		>
-			<Tab.Screen name='admin-home' component={AdminHomeScreen} />
+			<Tab.Screen
+				name='admin-dashboard'
+				component={AdminHomeScreen}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Icon name='stats-chart' color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='admin-waiting-list'
+				component={AdminWaitingListScreen}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Icon name='calendar-number' color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='admin-notification'
+				component={AdminNotificationScreen}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Icon name='notifications' color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='admin-profile'
+				component={AdminProfileScreen}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Icon name='person' color={color} size={size} />
+					),
+				}}
+			/>
 		</Tab.Navigator>
 	);
 };
---
+++ screens/admin/AdminNotificationScreen.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, View } from 'react-native';
+import CustomText from '../../components/CustomText';
+
+const AdminNotificationScreen = () => {
+	return (
+		<View>
+			<CustomText
+				message='This is Admin Home Page'
+				styles={styles.helloText}
+				variant='Medium'
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	helloText: {
+		fontSize: 30,
+		textAlign: 'center',
+	},
+});
+
+export default AdminNotificationScreen;
