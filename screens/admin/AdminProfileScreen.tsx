46143ae19dfb34f507ba93fb8f78586d30ff0b01
Hoang Anh <hoanganh.luong2906@gmail.com>
2024-06-25 06:34:38 +0700

    Admin dashboard

--- screens/admin/AdminProfileScreen.tsx
+++ screens/admin/AdminProfileScreen.tsx
@@ -1,7 +1,19 @@
-import { StyleSheet, View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
 import CustomText from '../../components/CustomText';
+import { RootDispatch } from '../../redux/configStore';
+import { useRootDispatch } from '../../redux/hooks';
+import { logout } from '../../redux/UserSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AdminProfileScreen = () => {
+	const dispatch: RootDispatch = useRootDispatch();
+
+	function handleLogout() {
+		dispatch(logout());
+		AsyncStorage.removeItem('token');
+		AsyncStorage.removeItem('user');
+	}
+
 	return (
 		<View>
 			<CustomText
@@ -9,6 +21,9 @@ const AdminProfileScreen = () => {
 				styles={styles.helloText}
 				variant='Medium'
 			/>
+			<Pressable onPress={handleLogout}>
+				<CustomText message='Logout' styles={styles.helloText} variant='Medium' />
+			</Pressable>
 		</View>
 	);
 };
