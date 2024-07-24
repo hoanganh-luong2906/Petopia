7907205ca3a47ebe61e34cdd3e7e27172e796985
Hoang Anh <hoanganh.luong2906@gmail.com>
2024-07-24 23:36:54 +0700

    💄 [fix]: Update response & minor changes

--- screens/user/MarketplaceProductScreen.tsx
+++ screens/user/MarketplaceProductScreen.tsx
@@ -8,12 +8,16 @@ import {
 import Icon from 'react-native-vector-icons/Ionicons';
 import CustomText from '../../components/CustomText';
 import {
+	COLOR_GRAY,
 	COLOR_PRIMARY_200,
 	COLOR_PRIMARY_900,
 	COLOR_SECONDARY_LIGHTER,
 	FONT_BOLD,
 	FONT_MEDIUM,
+	FONT_REGULAR,
+	FONT_SEMI_BOLD,
 	TEXT_LARGE,
+	TEXT_SECONDARY,
 } from '../../utils/Constants';
 
 interface IProps {
@@ -36,15 +40,15 @@ const MarketplaceProductScreen = (props: IProps) => {
 
 	const handleAddToFavourite = () => {
 		alert('Đã thêm vào danh sách yêu thích');
-	}
+	};
 
 	const handleShareProduct = () => {
 		alert('Đang chia sẻ sản phẩm');
-	}
+	};
 
 	const handleNotificationAnnounce = () => {
 		alert('Đang theo dõi thông báo');
-	 }
+	};
 
 	return (
 		<View style={styles.container}>
@@ -97,16 +101,83 @@ const MarketplaceProductScreen = (props: IProps) => {
 						/>
 					</>
 				</TouchableHighlight>
-				<View>
-					<TouchableHighlight
 
-					>
-						<Icon
-							name='heart-outline'
-							size={TEXT_LARGE + 2}
-							color={COLOR_PRIMARY_900}
+				<View style={styles.buttonContainer}>
+					<View style={styles.buttonContent}>
+						<TouchableHighlight
+							underlayColor={COLOR_SECONDARY_LIGHTER}
+							onPress={handleAddToFavourite}
+							style={styles.functionalButton}
+						>
+							<Icon
+								name='notifications'
+								size={TEXT_LARGE + 2}
+								color={'black'}
+							/>
+						</TouchableHighlight>
+						<CustomText
+							message='Thông báo'
+							variant={FONT_SEMI_BOLD}
+							styles={styles.buttonTitle}
 						/>
-					</TouchableHighlight>
+					</View>
+					<View style={styles.buttonContent}>
+						<TouchableHighlight
+							underlayColor={COLOR_SECONDARY_LIGHTER}
+							onPress={handleAddToFavourite}
+							style={styles.functionalButton}
+						>
+							<Icon name='heart' size={TEXT_LARGE + 2} color={'black'} />
+						</TouchableHighlight>
+						<CustomText
+							message='Lưu'
+							variant={FONT_SEMI_BOLD}
+							styles={styles.buttonTitle}
+						/>
+					</View>
+					<View style={styles.buttonContent}>
+						<TouchableHighlight
+							underlayColor={COLOR_SECONDARY_LIGHTER}
+							onPress={handleAddToFavourite}
+							style={styles.functionalButton}
+						>
+							<Icon
+								name='share-social'
+								size={TEXT_LARGE + 2}
+								color={'black'}
+							/>
+						</TouchableHighlight>
+						<CustomText
+							message='Chia sẻ'
+							variant={FONT_SEMI_BOLD}
+							styles={styles.buttonTitle}
+						/>
+					</View>
+				</View>
+
+				<View style={styles.descriptionContainer}>
+					<CustomText
+						message='Mô tả'
+						variant={FONT_SEMI_BOLD}
+						styles={styles.descriptionTitle}
+					/>
+					<CustomText
+						message='Ổ mèo bốn mùa thông dụng cho mèo con kiểu kín có thể tháo giặt cho mèo, giúp mèo luôn giữ ấm được cơ thể'
+						variant={FONT_REGULAR}
+						styles={styles.descriptionContent}
+					/>
+				</View>
+				<View style={styles.descriptionContainer}>
+					<CustomText
+						message='Thông tin người bán'
+						variant={FONT_SEMI_BOLD}
+						styles={styles.descriptionTitle}
+					/>
+					<CustomText
+						message='Ổ mèo bốn mùa thông dụng cho mèo con kiểu kín có thể tháo giặt cho mèo, giúp mèo luôn giữ ấm được cơ thể'
+						variant={FONT_REGULAR}
+						styles={styles.descriptionContent}
+					/>
 				</View>
 			</View>
 		</View>
@@ -127,7 +198,7 @@ const styles = StyleSheet.create({
 	headerContainer: {
 		width: '100%',
 		paddingHorizontal: wp(5),
-		paddingBottom: hp(1),
+		paddingBottom: hp(1.5),
 		borderBottomColor: 'lightgray',
 		borderBottomWidth: 1,
 	},
@@ -152,17 +223,70 @@ const styles = StyleSheet.create({
 	},
 	contactButton: {
 		width: '100%',
-		height: hp(6),
+		height: hp(6.5),
 		display: 'flex',
 		flexDirection: 'row',
 		justifyContent: 'center',
 		alignItems: 'center',
 		paddingHorizontal: wp(5),
 		marginVertical: hp(2),
-		borderRadius: 5,
+		borderRadius: 10,
 		borderColor: COLOR_PRIMARY_200,
 		borderWidth: 1,
 		columnGap: wp(2),
+	},
+	buttonContainer: {
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		paddingTop: hp(1),
+		paddingBottom: hp(2),
+		borderBottomColor: COLOR_GRAY,
+		borderBottomWidth: 1,
+	},
+	buttonContent: {
+		width: wp(20),
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	functionalButton: {
+		width: '55%',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		aspectRatio: 1,
+		borderRadius: 50,
+		backgroundColor: COLOR_GRAY,
+	},
+	buttonTitle: {
+		fontSize: TEXT_SECONDARY + 1,
+		paddingTop: hp(0.5),
+		textAlign: 'center',
+		letterSpacing: 0.5,
+	},
+	descriptionContainer: {
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+		paddingVertical: hp(1.5),
+		borderBottomColor: COLOR_GRAY,
+		borderBottomWidth: 1,
+	},
+	descriptionTitle: {
+		fontSize: TEXT_SECONDARY + 5,
+		letterSpacing: 0.2,
+	},
+	descriptionContent: {
+		width: '100%',
+		fontSize: TEXT_SECONDARY + 1,
+		textAlign: 'justify',
+		letterSpacing: 0.2,
 	},
 });
 
